9e53d8914c0df42b4387b8666c8d707b2b31b4a6
Lander Vitug <lander.vitug@gmail.com>
2024-08-02 10:33:45 -0500

    added alias in webpack config

--- webpack.common.js
+++ webpack.common.js
@@ -2,7 +2,7 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: "./src/index.js",
+  entry: "./src/js/index.js",
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "dist"),
@@ -21,15 +21,13 @@ module.exports = {
         test: /\.(woff|woff2|eot|ttf|otf)$/i,
         type: "asset/resource",
       },
-      {
-        test: /\.(csv|tsv)$/i,
-        use: ["csv-loader"],
-      },
-      {
-        test: /\.xml$/i,
-        use: ["xml-loader"],
-      },
     ],
+  },
+  resolve: {
+    alias: {
+      js: path.resolve(__dirname, "src/js"),
+      styles: path.resolve(__dirname, "src/styles"),
+    },
   },
   plugins: [
     new HtmlWebpackPlugin({
